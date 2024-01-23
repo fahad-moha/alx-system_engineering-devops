2e26dd5f1b327472f00087676c76b00983f5c271
fahad-moha <khiladifahad02@gmail.com>
2024-01-23 07:52:55 -0800

    chage config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
 #! /usr/bin/env bash
 # using puppet to make changes to our configuration file
 
-file {'ect/ssh/ssh_cofig':
+file { 'ect/ssh/ssh_cofig':
 	ensure => present,
 
 content =>"
 	#SSH client configuration
+	
 	host*
 	IdentityFile ~/.ssh/school
 	PasswordAuthentication no
