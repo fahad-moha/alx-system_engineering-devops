179a5fd41d231c871bc86d20bffdb2d64fd034f2
fahad-moha <khiladifahad02@gmail.com>
2024-03-06 20:51:43 -0800

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,6 +12,6 @@ def number_of_subscribers(subreddit):
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
     if sub_info.status_code >= 300:
-        return 0
+        return (0)
 
     return sub_info.json().get("data").get("subscribers")
